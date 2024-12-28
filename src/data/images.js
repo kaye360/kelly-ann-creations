cfbf7482b24051d222e08e914f9be331556818e4
joshkaye88 <jkaye@shaw.ca>
2024-12-27 22:58:35 -0700

    add gallery preview to home page

---
+++ src/data/images.js
@@ -0,0 +1,11 @@
+
+export const imageTitles = [
+    'A wedding cake for Rory and Fiona',
+    'Cupcakes for Sara',
+    'Cookies for a birthday party',
+    'Lindsay\'s 9th birthday cake',
+    'Cupcakes for a birthday party',
+    'Sarah and Tyler\'s wedding cake',
+    'A cake for a fall wedding',
+    'Cookies galore!',
+]
